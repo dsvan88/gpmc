0d0b8612a1323ea70f661cac35697d69ea705733
unknown <dsvan88@gmail.com>
2021-12-16 08:04:25 +0200

    v0.175 Small bugfix with Telegram

--- js/get_script.php
+++ js/get_script.php
@@ -4,5 +4,5 @@ $scriptType = isset($_GET['js']) ? 'js' : 'php';
 // if (in_array($scriptName,['game','get-data']))
 //     require_once './php/init.php';
 
-header('Content-Type: text/javascript');
+header('Content-Type: text/javascript; charset=utf-8');
 require "./scripts/$scriptType/{$_GET['js']}.$scriptType";
