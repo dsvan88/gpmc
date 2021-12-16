81febfe6f705ecb38433b1b5e0c4a273b79ae83d
unknown <dsvan88@gmail.com>
2021-12-16 23:42:49 +0200

    v0.176 Autocompress JS and pages HTML. For test. Turn-off GZIP for Heroku test

--- js/get_script.php
+++ js/get_script.php
@@ -5,7 +5,7 @@ $scriptType = isset($_GET['js']) ? 'js' : 'php';
 header('Content-Type: text/javascript; charset=utf-8');
 
 $script = preg_replace('/\/\*[^*]*\*+([^\/][^*]*\*+)*\//', '', file_get_contents($_SERVER['DOCUMENT_ROOT']."/js/scripts/$scriptType/$_GET[js].$scriptType"));
-$script = preg_replace('/\/\/.*?\r\n/', "", $script);
+$script = preg_replace('/\/\/.*?(\r\n|\r|\n)/', "", $script);
 $script = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $script);
 
 echo $script;
