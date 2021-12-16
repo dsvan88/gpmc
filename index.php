2a13acf41c7a704085ebc1c99f806d1b24dd8db6
unknown <dsvan88@gmail.com>
2021-12-16 23:23:08 +0200

    v0.176 Autocompress JS and pages HTML. For test. Turn-off GZIP for Heroku test

--- index.php
+++ index.php
@@ -108,4 +108,5 @@ $outputHtml = str_replace(array_keys($output),array_values($output),$template);
 // header('Content-Length: ' . mb_strlen($outputHtml,'UTF-8'));
 $test = gzencode($outputHtml,1);
 file_put_contents($_SERVER['DOCUMENT_ROOT'].'/gzipped.txt', $test);
+file_put_contents($_SERVER['DOCUMENT_ROOT'].'/not_gzipped.txt', $outputHtml);
 echo $outputHtml;
