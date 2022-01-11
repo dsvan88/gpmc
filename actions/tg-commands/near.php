c35fb5086e594ebd5409323810d21ff81dcd26b1
dsvan88 <dsvan88@gmail.com>
2022-01-11 16:02:14 +0200

    v0.18 Small CSS bugfix and Adjust /near command

--- actions/tg-commands/near.php
+++ actions/tg-commands/near.php
@@ -6,6 +6,12 @@ $weeks = new Weeks;
 
 $weekData = $weeks->getDataByTime();
 
+$gameNames = [
+    'mafia' => 'Мафия',
+    'poker' => 'Покер',
+    'cash' => 'Кеш-покер'
+];
+
 if ($weekData) {
     $output['message'] = '';
     for ($i = 0; $i < 7; $i++) {
@@ -17,7 +23,7 @@ if ($weekData) {
             ['<b>Понедельник</b>', '<b>Вторник</b>', '<b>Среда</b>', '<b>Четверг</b>', '<b>Пятница</b>', '<b>Суббота</b>', '<b>Воскресенье</b>'],
             date('d.m.Y (l)', $weekData['start'] + 86400 * $i)
         ) . ' ' . $weekData['data'][$i]['time'];
-        $output['message'] .= "$date - {$weekData['data'][$i]['game']}\r\n\r\n";
+        $output['message'] .= "$date - {$gameNames[$weekData['data'][$i]['game']]}\r\n\r\n";
         for ($x = 0; $x < count($weekData['data'][$i]['participants']); $x++) {
             $output['message'] .= ($x + 1) . ". <b>{$weekData['data'][$i]['participants'][$x]['name']}</b> {$weekData['data'][$i]['participants'][$x]['arrive']}\r\n";
         }
