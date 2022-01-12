a51b627e6177a1d0bea6f7f55e0f4fae38d0a99d
unknown <dsvan88@gmail.com>
2022-01-12 19:09:32 +0200

    v0.182 Final test tg commands

--- actions/tg-commands/near.php
+++ actions/tg-commands/near.php
@@ -46,9 +46,9 @@ if ($weekData) {
         $output['message'] .= "$date - {$gameNames[$weekData['data'][$i]['game']]} ({$costs[$weekData['data'][$i]['game']]})\r\n";
 
         if (in_array('fans', $weekData['data'][$i]['mods'], true))
-            $output['message'] .= "*ФАНОВАЯ! Хорошо проведите время и повеселитесь!\r\n";
+            $output['message'] .= "*<i>ФАНОВАЯ</i>! Хорошо проведите время и повеселитесь!\r\n";
         if (in_array('tournament', $weekData['data'][$i]['mods'], true))
-            $output['message'] .= "ТУРНИР! Станьте чемпионом в равой борьбе!\r\n";
+            $output['message'] .= "<i>ТУРНИР</i>! Станьте чемпионом в равной борьбе!\r\n";
 
         $output['message'] .= "\r\n";
 
