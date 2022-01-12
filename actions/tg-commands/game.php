573f50fdd28a47e32c3079885d0341e5957cdffb
dsvan88 <dsvan88@gmail.com>
2022-01-12 10:27:54 +0200

    v0.182 Rebuild and test tg command game

--- actions/tg-commands/game.php
+++ actions/tg-commands/game.php
@@ -59,20 +59,20 @@ if (!isset($userData['id'])) {
             $requestData['duration'] = substr($value, 0, 1);
         }
     }
+    $output['message'] = json_encode($requestData, JSON_UNESCAPED_UNICODE) . ' ' . $dayName . ' ' . $withoutMethod;
+    // $currentDay = getdate()['wday'] - 1;
 
-    $currentDay = getdate()['wday'] - 1;
+    // if ($currentDay === -1)
+    //     $currentDay = 6;
 
-    if ($currentDay === -1)
-        $currentDay = 6;
-
-    $output['message'] = json_encode($requestData, JSON_UNESCAPED_UNICODE);
-    if ($currentDay > $requestData['dayNum']) {
-        $output['message'] = 'Не могу записать Вас на уже прошедший день! Sowwy:(';
-    } else {
-        if ($requestData['method'] === '-') {
-            $output['message'] = $weeks->dayUserUnregistrationByTelegram($requestData);
-        } else {
-            $output['message'] = $weeks->dayUserRegistrationByTelegram($requestData);
-        }
-    }
+    // $output['message'] = json_encode($requestData, JSON_UNESCAPED_UNICODE);
+    // if ($currentDay > $requestData['dayNum']) {
+    //     $output['message'] = 'Не могу записать Вас на уже прошедший день! Sowwy:(';
+    // } else {
+    //     if ($requestData['method'] === '-') {
+    //         $output['message'] = $weeks->dayUserUnregistrationByTelegram($requestData);
+    //     } else {
+    //         $output['message'] = $weeks->dayUserRegistrationByTelegram($requestData);
+    //     }
+    // }
 }
