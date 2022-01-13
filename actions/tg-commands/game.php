f9bacc0962827d4ef0be26e111fa19f19133c24d
dsvan88 <dsvan88@gmail.com>
2022-01-13 10:52:29 +0200

    v0.182 Update help Telegram command

--- actions/tg-commands/game.php
+++ actions/tg-commands/game.php
@@ -73,7 +73,7 @@ if (!isset($userData['id'])) {
 
     $output['message'] = json_encode($requestData, JSON_UNESCAPED_UNICODE);
     if ($currentDay > $requestData['dayNum']) {
-        $output['message'] = 'Не могу записать Вас на уже прошедший день! Sowwy:( ';
+        $output['message'] = 'Не могу записать Вас на уже прошедший день! Sowwy:(';
     } else {
         if ($requestData['method'] === '-') {
             $output['message'] = $weeks->dayUserUnregistrationByTelegram($requestData);
