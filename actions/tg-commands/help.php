45b53cb8b6b4c2f2d7e5a53737bf20b830523764
unknown <dsvan88@gmail.com>
2022-01-16 14:39:42 +0200

    v0.191 update /help telegram command

--- actions/tg-commands/help.php
+++ actions/tg-commands/help.php
@@ -11,3 +11,17 @@ $output['message'] = "<i>Инструкция к боту</i>.\r\n
     <u>/nick Ваш псевдоним</u> (кириллицей) <i>// Зарегистрировать свой псевдоним</i>
     <u>/?</u> или <u>/help</u> <i>// Это меню</i>
 ";
+if ($_POST['message']['chat']['type'] !== 'private') {
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/engine/class.users.php';
+
+    $users = new Users;
+
+    $telegramId = $_POST['message']['from']['id'];
+
+    $userData = $users->usersGetData(['id', 'status'], ['telegramid' => $telegramId]);
+    if (in_array($userData['status'], ['admin', 'manager'])) {
+        $output['message'] = "\r\nКоманды админа:
+    /reg +вс, Псевдоним, 18:00, 1-2 игры
+";
+    }
+}
