0174c706d253e03125d52e65e57681f521364c8d
dsvan88 <dsvan88@gmail.com>
2022-01-24 12:13:26 +0200

    v0.193 Test week-list for monday's bugfix

--- engine/migrate.php
+++ engine/migrate.php
@@ -12,6 +12,8 @@ $action->query(
     "
     )
 ); */
+
+/* 
 $action->rowDelete(5, SQL_TBLWEEKS);
 
 $weeksData = $action->getAssocArray(
@@ -42,4 +44,21 @@ $weeksData = $action->getAssocArray(
         )
     )
 );
-print_r($weeksData);
+ */
+
+$weeksData = $action->getAssocArray(
+    $action->query(
+        str_replace(
+            '{SQL_TBLWEEKS}',
+            SQL_TBLWEEKS,
+            "SELECT id,start,finish FROM {SQL_TBLWEEKS} ORDER BY id"
+        )
+    )
+);
+foreach ($weeksData as $index => $data) {
+    $mondayStart = date('H', $data['start']);
+    if ($mondayStart > 0) {
+        $mondayStart = $data['start'] - (60 * 60 * $mondayStart) + 1;
+        $action->rowUpdate(['start' => $mondayStart], ['id' => $data['id']], SQL_TBLWEEKS);
+    }
+}
