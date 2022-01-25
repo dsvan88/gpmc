9d168487066382e051f756ab11aa21219aa06010
dsvan88 <dsvan88@gmail.com>
2022-01-25 10:58:35 +0200

    v0.194 Test game tg command for user registration on future week

--- engine/class.weeks.php
+++ engine/class.weeks.php
@@ -193,7 +193,7 @@ class Weeks
 			'duration'	=> 	(int) $data['duration']
 		];
 
-		return json_encode($newData, JSON_UNESCAPED_UNICODE);
+		// return json_encode($newData, JSON_UNESCAPED_UNICODE);
 
 		$result = $this->daySetApproved($newData);
 
