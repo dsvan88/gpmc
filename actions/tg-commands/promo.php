227c581e393b909bdf0f9f35f1bdea8d420558ea
unknown <dsvan88@gmail.com>
2022-01-27 20:30:03 +0200

    v0.196 Begin promo block building

--- actions/tg-commands/promo.php
+++ actions/tg-commands/promo.php
@@ -1,7 +1,18 @@
 <?
 $promoText = trim(mb_substr($_POST['message']['text'], $commandLen + 1, NULL, 'UTF-8'));
 preg_match('/(.*?)\n(.*?)\n([^`]*)/', $promoText, $matches);
-$output['message'] .= $promoText;
+
+$entities = [];
+for ($i = 0; $i < count($_POST['message']['entities']); $i++) {
+    if ($_POST['message']['entities'][$i]['type'] === 'bot_command') continue;
+    $entities[$_POST['message']['entities'][$i]['type']] = [
+        'offset' => $_POST['message']['entities'][$i]['offset'],
+        'length' => $_POST['message']['entities'][$i]['length'],
+        'text' => mb_substr($_POST['message']['text'], $_POST['message']['entities'][$i]['offset'], $_POST['message']['entities'][$i]['length'])
+    ];
+}
+
+$output['message'] .= json_encode($entities, JSON_UNESCAPED_UNICODE);
 $output['message'] .= "\r\n";
 $output['message'] .= json_encode($matches, JSON_UNESCAPED_UNICODE);
 $output['message'] .= "\r\n";
