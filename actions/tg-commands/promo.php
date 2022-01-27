b523502609dde6bf665c4e800a97154abadcac57
unknown <dsvan88@gmail.com>
2022-01-27 21:58:23 +0200

    v0.196 Begin promo block building

--- actions/tg-commands/promo.php
+++ actions/tg-commands/promo.php
@@ -1,4 +1,9 @@
 <?
+if (!isset($_POST['message'])) {
+    $_POST = json_decode('{"update_id":314931246,"message":{"message_id":626,"from":{"id":900669168,"is_bot":false,"first_name":"Dmytro","last_name":"Vankevych","username":"dsvan88","language_code":"ru"},"chat":{"id":900669168,"first_name":"Dmytro","last_name":"Vankevych","username":"dsvan88","type":"private"},"date":1643312770,"text":"\/promo тест тайтл\nтест субтайтл\nТест текста, строка 1\nText test, string 2\nТест текста, строка 3","entities":[{"offset":0,"length":6,"type":"bot_command"},{"offset":12,"length":5,"type":"bold"},{"offset":23,"length":8,"type":"italic"},{"offset":37,"length":6,"type":"bold"},{"offset":45,"length":6,"type":"italic"}]}}', true);
+    $test = true;
+}
+
 $promoText = trim(mb_substr($_POST['message']['text'], $commandLen + 1, NULL, 'UTF-8'));
 preg_match('/(.*?)\n(.*?)\n([^`]*)/', $promoText, $matches);
 
@@ -28,6 +33,11 @@ if (isset($_POST['message']['entities'])) {
     if ($newString !== '')
         $output['message'] .= $newString;
 }
+
+if (!$test) {
+    print_r($output);
+}
+
 /* $output['message'] .= "\r\n";
 $output['message'] .= json_encode($matches, JSON_UNESCAPED_UNICODE);
 $output['message'] .= "\r\n";
