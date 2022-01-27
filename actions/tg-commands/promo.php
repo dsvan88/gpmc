6b27d4e27aeefff3e5e741a2952861118e9c3f88
unknown <dsvan88@gmail.com>
2022-01-27 19:57:11 +0200

    v0.196 Begin promo block building

--- actions/tg-commands/promo.php
+++ actions/tg-commands/promo.php
@@ -1,6 +1,8 @@
 <?
-$promoText = trim(mb_substr($_POST['message']['text'], $commandLen + 1, NULL, 'UTF-8'));
+$promoText = trim(mb_substr($_POST['message']['text'], $commandLen, NULL, 'UTF-8'));
 preg_match('/^(.*)\n(.*)\n(.*)$/', $promoText, $matches);
+$output['message'] .= $promoText;
+$output['message'] .= "\r\n";
 $output['message'] .= json_encode($matches, JSON_UNESCAPED_UNICODE);
 $output['message'] .= "\r\n";
 $output['message'] .= json_encode($_POST, JSON_UNESCAPED_UNICODE);
