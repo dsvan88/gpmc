f13159e94cad21b0ade0d7dd2dd817d4a6df94ef
dsvan88 <dsvan88@gmail.com>
2022-01-31 11:56:34 +0200

    v0.197 Update Modal and header CSS

--- actions/tg-commands/promo.php
+++ actions/tg-commands/promo.php
@@ -46,5 +46,3 @@ if ($newsData) {
 }
 
 $output['message'] = 'Промо-блок, успішно збережений!';
-file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/tg-message.txt', print_r($_POST['message'], true));
-file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/tg-error.txt', print_r($array, true));
